53801098d96f9a4e8f97c6a0cdab4d5042ca1c9e
Jossif <jossif.elefteriadis@bonniernews.se>
2021-09-21 21:23:20 +0200

    Add two comp and test that they work

---
+++ nftpedia/src/components/StartPage.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Navbar from "./Navbar";
+
+export default class StartPage extends React.Component {
+  render() {
+    return (
+      <section>
+        <Navbar />
+      </section>
+    );
+  }
+}
