2f77e0205db5b42509f6c5487c8f14df1d4495a2
Jossif <jossif.elefteriadis@bonniernews.se>
2021-09-28 19:45:15 +0200

    Move Navbar import to App.js

--- nftpedia/src/App.js
+++ nftpedia/src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import "./App.css";
+import Navbar from "./components/Navbar";
 import PageContainer from "./components/PageContainer";
 
 function App() {
   return (
     <section className="App">
+      <Navbar />
       <PageContainer />
     </section>
   );
--- nftpedia/src/components/PageContainer.js
+++ nftpedia/src/components/PageContainer.js
@@ -1,12 +1,7 @@
 import React from "react";
-import Navbar from "./Navbar";
 
 export default class StartPage extends React.Component {
   render() {
-    return (
-      <section>
-        <Navbar />
-      </section>
-    );
+    return <section></section>;
   }
 }
