97ad46fc571b2db1fd718b9d64e817475c037140
Jossif <jossif.elefteriadis@bonniernews.se>
2021-09-28 19:33:42 +0200

    remove default react code and add startPage section

--- nftpedia/src/App.js
+++ nftpedia/src/App.js
@@ -1,26 +1,12 @@
-import logo from "./logo.svg";
+import React from "react";
 import "./App.css";
 import StartPage from "./components/Navbar";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <StartPage />
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section className="App">
+      <StartPage />
+    </section>
   );
 }
 
