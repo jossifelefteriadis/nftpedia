7a95c0baa5fde1af7f39c3dc623542d9b60bb411
Jossif <jossif.elefteriadis@bonniernews.se>
2021-10-18 17:25:18 +0200

    changed classname

--- nftpedia/src/components/StartPage.js
+++ nftpedia/src/components/StartPage.js
@@ -4,7 +4,7 @@ import beepleNft from "../images/beeple_nft.jpeg";
 
 const StartPage = () => (
   <section className="startpage">
-    <section className="col">
+    <section className="startpage__box">
       {/* <p>Den ena en bild på några nft's med rubrik nft historia</p> */}
       <img src={beepleNft} alt="TEAM WHALE" className="firstPage_img" />
       <p className="startpage__p--title">HISTORIAN BAKOM NFT</p>
@@ -14,14 +14,14 @@ const StartPage = () => (
         andra förmögna personer valt att ge sig in i marknaden.
       </p>
     </section>
-    <section className="col">
+    <section className="startpage__box">
       2
       <p>
         En bild/länken kan handla om 'varför nft' och olika användningsområden
         (pfp, play earn, yatch club, osv)
       </p>
     </section>
-    <section className="col">3</section>
+    <section className="startpage__box">3</section>
   </section>
 );
 
