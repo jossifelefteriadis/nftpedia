47d1e503caba0305562c75e7ac762668be7e543b
Jossif Elefteriadis <jossif.elefteriadis@bonniernews.se>
2021-10-22 20:17:06 +0200

    fix npm sync problems

---
+++ src/components/About.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const About = () => (
+  <section className="about">
+    <p>Denna sida är en om oss sida.</p>
+  </section>
+);
+
+export default About;
---
+++ src/components/PageContainer.js
@@ -0,0 +1,39 @@
+import React from "react";
+import { Route, Switch } from "react-router-dom";
+// , Link, Switch
+
+import Alphabet from "./Alphabet";
+import About from "./About";
+
+import StartPage from "./StartPage";
+// import Airdrop from "./words/Airdrop";
+
+export default class PageContainer extends React.Component {
+  render() {
+    return (
+      <section className="pagecontainer">
+        <Switch>
+          {/* <Alphabet /> */}
+          {/* <Route path="/airdrop">
+            <Airdrop />
+          </Route> */}
+          <Route path="/omoss">
+            <About />
+          </Route>
+          <Route path="/">
+            <Alphabet />
+            <StartPage />
+          </Route>
+
+          {/* <p>3 kolumner.</p>
+          <p>
+            En bild/länken kan handla om Vad är NFT (eller om detta är en
+            paragraf)
+          </p>
+          <p>En bild/länken kan handla om olika wallets</p>
+          <p>En bild/länken kan handla networks med smart contracts</p> */}
+        </Switch>
+      </section>
+    );
+  }
+}
