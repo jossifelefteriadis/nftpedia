61d623c33968fdfeadcbfc9be804537188d7e441
Jossif Elefteriadis <jossif.elefteriadis@bonniernews.se>
2021-10-29 17:19:41 +0200

    [WIP] Add words to letter A

---
+++ src/components/letter/A.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const A = () => (
+  <section className="letter">
+    <h1>A</h1>
+    <p className="letter--word">AIRDROP</p>
+    <br />
+    <p className="letter--word">APE-IN</p>
+  </section>
+);
+
+export default A;
