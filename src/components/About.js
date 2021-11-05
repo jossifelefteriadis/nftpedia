fd91e0b25414e67f1e58f9120b66fd58f627fd66
Jossif Elefteriadis <jossif.elefteriadis@bonniernews.se>
2021-11-05 16:20:48 +0100

    [WIP] Add text and styling for about page

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,32 @@ import React from "react";
 
 const About = () => (
   <section className="about">
-    <p>Denna sida är en om oss sida.</p>
+    <h2>OM NFTPEDIA</h2>
+    <p>
+      NFTPEDIA skapades eftersom efterfrågan på NFT (non-fungible token) har
+      ökat väldigt mycket sen början av 2021 och eftersom majoriteten av de som
+      investerat/känner till begreppet/är intresserade av NFT inte har så bra
+      koll på alla begrepp.
+    </p>
+    <p>
+      Vi är en trovärdig och transparent källa, och försöker alltid förbättra
+      och utöka våra beskrivningar och förklaringar inom NFT-världen.
+    </p>
+    <br />
+    <br />
+    <h2>VILKA ÄR VI?</h2>
+    <p>
+      Jag som tagit fram, och driver NFTPEDIA heter Jossif Elefteriadis och är
+      en fullstack utvecklare med stort intresse inom bland annat krypto- och
+      NFT-världen. Målet med NFTPEDIA är att, allt eftersom NFT's blir mer
+      populära och mer användbara i vår vardag, möta det framtida behovet att
+      kunna första och fördjupa sig inom exempelvis begreppen som finns.
+      <br />
+      <br />
+      Vill du bidra till att förbättra ett begrepps beskrivning, eller saknar du
+      en term? Hör gärna av dig via twitter så ser vi till att den kommer med på
+      hemsidan.
+    </p>
   </section>
 );
 
