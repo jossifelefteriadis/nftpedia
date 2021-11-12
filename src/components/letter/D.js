0f61bbddf2234270648f1f689a4aef7a321f3a37
Jossif Elefteriadis <jossif.elefteriadis@bonniernews.se>
2021-11-12 22:14:13 +0100

    [WIP] words for letter D

---
+++ src/components/letter/D.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const D = () => (
+  <section className="words">
+    <h1>D</h1>
+    <br />
+    <p className="letter--word">
+      <Link to="/DECENTRALAND">DECENTRALAND</Link>
+    </p>
+    <p className="letter--word">
+      <Link to="/DECENTRALIZED">DECENTRALIZED</Link>
+    </p>
+    <p className="letter--word">
+      <Link to="/DIAMOND-HANDS">DIAMON HANDS</Link>
+    </p>
+    <p className="letter--word">
+      <Link to="/DROP">DROP</Link>
+    </p>
+    <p className="letter--word">
+      <Link to="/DUNE-ANALYTICS">DUNE ANALYTICS</Link>
+    </p>
+  </section>
+);
+
+export default D;
