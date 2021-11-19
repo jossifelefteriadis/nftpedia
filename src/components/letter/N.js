1041ee6884525124c95fd90c5b4bb25908cede48
Jossif Elefteriadis <jossif.elefteriadis@bonniernews.se>
2021-11-19 19:03:27 +0100

    Add words for letter N

---
+++ src/components/letter/N.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const N = () => (
+  <section className="words">
+    <h1>N</h1>
+    <br />
+    <p className="letter--word">
+      <Link to="/NON-FUNGIBLE-TOKEN">NON-FUNGIBLE TOKEN</Link>
+    </p>
+  </section>
+);
+
+export default N;
