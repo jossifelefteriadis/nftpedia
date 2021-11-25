36243db13390bfed2204ad86f5e0f8d0ae5d0b8a
Jossif Elefteriadis <jossif.elefteriadis@bonniernews.se>
2021-11-25 21:01:18 +0100

    Refactor

--- src/components/words/Block.js
+++ src/components/words/Block.js
@@ -4,7 +4,7 @@ const Block = () => (
   <section className="word-description-container">
     <h1>Block</h1>
     <p>
-      En Block kan definieras som en enda digital post skapad inom en
+      En Block kan definieras som en enda digital post skapad inom en{" "}
       <a href="https://www.nftpedia.se/blockchain">blockchain</a>. Varje block
       är i princip permanent information på blockchain. Varje gång ett block är
       'färdigt', gör den plats åt nästa block.
