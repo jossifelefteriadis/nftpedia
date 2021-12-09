5c36bc9393922fddc2ef394fa65096a68895c46b
Jossif Elefteriadis <jossif.elefteriadis@bonniernews.se>
2021-12-09 19:35:26 +0100

    Add description for metamask

---
+++ src/components/words/Metamask.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Metamask = () => (
+  <section className="word-description-container">
+    <h1>Metamask</h1>
+    <p>
+      Metamask är en kryptoplånbok som används för att interagera med Ethereum
+      blockchain. Via en extension på webbläsaren eller en app i telefonen,
+      tillåter den mot decentraliserade appar.
+      <br />
+      <br />
+      <br />
+      <b>Officiell hemsida:</b> https://metamask.io/
+    </p>
+  </section>
+);
+
+export default Metamask;
