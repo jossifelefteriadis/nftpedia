534a282b83f967aacd7686f41c663b94d861f232
Jossif Elefteriadis <jossif.elefteriadis@bonniernews.se>
2021-12-09 17:48:14 +0100

    Add description for Mainnet

---
+++ src/components/words/Mainnet.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+const Mainnet = () => (
+  <section className="word-description-container">
+    <h1>Mainnet</h1>
+    <p>
+      Mainnet är det primära publika blockchian där mestadels av all aktivitet
+      äger rum.
+      <br />
+      <br />
+      Till skillnad från testnet som är till för att utvecklare ska kunna testa
+      sina smart contracts innan man börjar leverera mot mainnet.
+      <br />
+      <br />
+      'Rinkeby' är exempel på testnet för Ethereum. Kusama är testnet för
+      Polkadot.
+    </p>
+  </section>
+);
+
+export default Mainnet;
