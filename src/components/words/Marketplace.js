8ce72248982261e3ab9f922043e99d93d733e7c0
Jossif Elefteriadis <jossif.elefteriadis@bonniernews.se>
2021-12-09 19:23:25 +0100

    Add description to marketplace

---
+++ src/components/words/Marketplace.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+const Marketplace = () => (
+  <section className="word-description-container">
+    <h1>Marketplace</h1>
+    <p>
+      NFT marketplaces (marknadsplatser) är plattformar där NFTs kan skapas,
+      visas upp, eller handlas. Nedan finns en lista med populära marketplaces
+      för NFTs.
+      <br />
+      <br />
+      <br />
+      <b>OpenSea:</b> https://opensea.io/
+      <b>Rarible:</b> https://rarible.com/
+      <b>HeN:</b> https://www.hicetnunc.xyz/
+      <br />
+      <br />
+      <b>NiftyGateway:</b> https://niftygateway.com/
+      <b>Superrare:</b> https://superrare.com/
+      <b>Maker's Place:</b> https://makersplace.com/
+      <b>Foundation:</b> https://foundation.app/
+    </p>
+  </section>
+);
+
+export default Marketplace;
