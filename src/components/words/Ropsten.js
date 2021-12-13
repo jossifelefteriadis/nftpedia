9b647b63fbb0b16443d90ba84de8a5d34d883b2e
Jossif Elefteriadis <jossif.elefteriadis@bonniernews.se>
2021-12-13 18:22:14 +0100

    Add description to words

---
+++ src/components/words/Ropsten.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Ropsten = () => (
+  <section className="word-description-container">
+    <h1>ROPSTEN</h1>
+    <p>
+      Ropsten är ett Ethereum testnet. Testnets används oftast av utvecklare för
+      att bland annat testa sina smart contracts.
+    </p>
+  </section>
+);
+
+export default Ropsten;
---
+++ src/components/words/RugPull.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const RugPull = () => (
+  <section className="word-description-container">
+    <h1>RUG PULL</h1>
+    <p>
+      Rug Pull är oftast en exit från en tydlig scam. Generellt så brukar
+      skaparna försvinna och överge projektet direkt efter att man sålt ut allt,
+      med alla investerares pengar utan någon tanke att komma tillbaka och
+      fortsätta utveckla projektet.
+    </p>
+  </section>
+);
+
+export default RugPull;
